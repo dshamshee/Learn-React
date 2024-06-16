faeec2ccc4fff2c9070d528992f2cce7ef5c5923
Danish Shamshee <danishshamshee123@gmail.com>
2024-06-16 23:48:15 +0530

    Creake Own React Library and JSX

--- 01vitreact/src/main.jsx
+++ 01vitreact/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>Welocme to Custom React</h1>
+    </div>
+  )
+}
+
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: "https://google.com",
+//       target: "_blank"
+//   },
+//   children: "Clisk me to visit Google"
+// }
+
+const CustomElement = (
+  <a href="https://google.com" target='_blank'>Click me visit Google</a>
+)
+
+const CustomElement2 = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click Me to visit Google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ 
+    // <MyApp />
+    // CustomElement
+    CustomElement2
+ 
 )
---
+++ customReact/customReact.js
@@ -0,0 +1,34 @@
+
+function customeReact(reactElement, container){
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+    // container.appendChild(domElement)
+
+
+// Second Approach (recomended)
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+        // console.log(prop)
+        // console.log(reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "Clisk me to visit Google"
+}
+
+const mainContainer = document.querySelector('#root')
+
+customeReact(reactElement, mainContainer)
