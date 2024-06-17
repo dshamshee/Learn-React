0636ab268f7ad02abb3fc6e8d9e8941e45c70d09
Danish Shamshee <danishshamshee123@gmail.com>
2024-06-17 22:15:39 +0530

    Create Counter using Hooks

---
+++ counter_Using_Hooks/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+let [count, setCounter] = useState(0)
+
+  const addValue = ()=>{
+    // count = count +1
+    if(count<20){
+
+      setCounter(count + 1)
+    }else alert('you do not add more value')
+  }
+
+  const removeValue = ()=>{
+    if(count>0){
+
+      setCounter(count - 1)
+    }else alert('You do not remove more value')
+  }
+
+  // let count = 0
+
+  return (
+    <>
+        <h1>React Counter</h1>
+        <h2>Counter {count}</h2>
+        <button onClick={addValue}>Add Value {count}</button>
+        <br /><br />
+        <button onClick={removeValue}>Remove Value {count}</button>
+    </>
+  )
+}
+
+export default App
