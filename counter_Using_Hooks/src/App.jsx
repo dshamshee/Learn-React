f623d9e1da24038e6899ed96bb3ec65b7b341bd8
Danish Shamshee <danishshamshee123@gmail.com>
2024-06-18 20:26:07 +0530

    A scenario which is mostly asked in interview

--- counter_Using_Hooks/src/App.jsx
+++ counter_Using_Hooks/src/App.jsx
@@ -5,32 +5,41 @@ import './App.css'
 
 function App() {
 
-let [count, setCounter] = useState(0)
+  let [count, setCounter] = useState(0)
 
-  const addValue = ()=>{
+  const addValue = () => {
     // count = count +1
-    if(count<20){
+    if (count < 20) {
 
-      setCounter(count + 1)
-    }else alert('you do not add more value')
+      // setCounter(count + 1)
+
+
+      setCounter((prevCounter) => prevCounter + 1)  // the callBack function return a previous <counter> value, and here we get that previous value and update it by 1
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+
+
+
+    } else alert('you do not add more value')
   }
 
-  const removeValue = ()=>{
-    if(count>0){
+  const removeValue = () => {
+    if (count > 0) {
 
       setCounter(count - 1)
-    }else alert('You do not remove more value')
+    } else alert('You do not remove more value')
   }
 
   // let count = 0
 
   return (
     <>
-        <h1>React Counter</h1>
-        <h2>Counter {count}</h2>
-        <button onClick={addValue}>Add Value {count}</button>
-        <br /><br />
-        <button onClick={removeValue}>Remove Value {count}</button>
+      <h1>React Counter</h1>
+      <h2>Counter {count}</h2>
+      <button onClick={addValue}>Add Value {count}</button>
+      <br /><br />
+      <button onClick={removeValue}>Remove Value {count}</button>
     </>
   )
 }
