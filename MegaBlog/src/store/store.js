0763b5acc35c02a3cbbf4309f9e329c8026f8417
Danish Shamshee <danishshamshee123@gmail.com>
2024-07-02 23:03:26 +0530

    state management using reactRedux

--- MegaBlog/src/store/store.js
+++ MegaBlog/src/store/store.js
@@ -1,9 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
-
+import authSlice from './authSlice'
+// import authSlice from './authSlice.js';
 const store = configureStore({
     reducer: {
-        
+        authSlice
     }
 })
 
-export default store;
+export default store;  
