41cc601df8521a53146aa6ffb88d17f78a38cdff
Danish Shamshee <danishshamshee123@gmail.com>
2024-07-09 23:22:42 +0530

    modify index.js file

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -5,6 +5,8 @@ import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 import Button from './Button.jsx'
 import Input from './Input.jsx'
+import Select from './Select.jsx'
+import RTE from "./RTE.jsx";
 export {
     Header,
     Footer,
@@ -12,5 +14,7 @@ export {
     Logo,
     LogoutBtn,
     Button,
-    Input
+    Input,
+    Select,
+    RTE
 }
